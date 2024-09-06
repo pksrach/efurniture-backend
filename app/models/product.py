bb839c8beed50339f0067aaaeb3d9fca06eb65e3
Meas Manet <mrneth58@gmail.com>
2024-09-06 19:09:59 +0700

    update model and add migration

--- app/models/product.py
+++ app/models/product.py
@@ -1,18 +1,23 @@
-from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String,Float
+# Product model
+from sqlalchemy import Boolean, Column, ForeignKey, String, Float
 from app.config.database import Base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 from sqlalchemy.orm import relationship
 
 class Product(Base):
-    __tablename__ = "products"
+    __tablename__ = 'products'
+    
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     description = Column(String)
     attachment = Column(String)
     is_active = Column(Boolean, default=True)
-    category_id = Column(UUID(as_uuid=True), ForeignKey("categories.id"))
-    brand_id = Column(UUID(as_uuid=True), ForeignKey("brands.id"))
+    
+    # Correct the ForeignKey to reference categories.category_id
+    category_id = Column(UUID(as_uuid=True), ForeignKey("categories.category_id"))  # Correct column name
+    
+    brand_id = Column(UUID(as_uuid=True), ForeignKey("brands.brand_id"))
 
     category = relationship("Category", back_populates="products")
     brand = relationship("Brand", back_populates="products")
--- app/models/product_price.py
+++ app/models/product_price.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, String,Float
+from sqlalchemy import Column, ForeignKey, String, Float
 from app.config.database import Base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
@@ -8,10 +8,10 @@ class ProductPrice(Base):
     __tablename__ = "product_prices"
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     price = Column(Float)
-    color_id = Column(UUID(as_uuid=True), ForeignKey("colors.id"))
+    color_id = Column(UUID(as_uuid=True), ForeignKey("colors.color_id"))  # Reference the correct column
     size = Column(String)
     product_id = Column(UUID(as_uuid=True), ForeignKey("products.id"))
 
-    color = relationship("Color")
+    color = relationship("Category")  # Use 'Category' because that's the class name
     product = relationship("Product", back_populates="product_prices")
     order_details = relationship("OrderDetail", back_populates="product_price")
