22d558ca22236f0b74f2803994b4c13cd3b1d49b
pksrach <pksrach@gmail.com>
2024-09-19 02:20:18 +0700

    Reduce code on create and update product

--- app/services/product.py
+++ app/services/product.py
@@ -52,126 +52,56 @@ async def get_product(id: str, session: AsyncSession) -> ProductResponse:
 
 async def create_product(req: ProductRequest, session: AsyncSession) -> ProductResponse:
     try:
-        # Validate category_id
-        stmt = select(Category).where(Category.id == req.category_id)
-        result = await session.execute(stmt)
-        category = result.scalar()
-        if not category:
+        await validate_category(req.category_id, session)
+        await validate_brand(req.brand_id, session)
+
+        if await validate_product_name(req.name, session):
             return ProductResponse(
                 data=None,
-                message="Invalid category_id. Category does not exist."
+                message="Product already exists"
             )
 
-        # Validate brand_id
-        stmt = select(Brand).where(Brand.id == req.brand_id)
-        result = await session.execute(stmt)
-        brand = result.scalar()
-        if not brand:
-            return ProductResponse(
-                data=None,
-                message="Invalid brand_id. Brand does not exist."
-            )
-
-        # Check if a product with the same name exists
-        stmt = select(Product).options(
-            selectinload(Product.category),
-            selectinload(Product.brand),
-            selectinload(Product.product_prices).selectinload(ProductPrice.color)  # Load the color for product prices
-        ).where(Product.name == req.name)
-        result = await session.execute(stmt)
-        product = result.scalar()
-
-        if product:
-            return ProductResponse(
-                data=ProductDataResponse.from_entity(product),
-                message=f"Product already exists"
-            )
-
-        # Create the new product
         product = Product(
             name=req.name,
             description=req.description,
             attachment=req.attachment,
             category_id=req.category_id,
-            brand_id=req.brand_id,
+            brand_id=req.brand_id
         )
         session.add(product)
-        await session.flush()  # Flush to get product.id populated without committing
+        await session.flush()
 
-        # Validate product_prices not empty
-        if not req.product_prices:
-            logging.error("Product prices cannot be empty", exc_info=True)
-            return ProductResponse(
-                data=None,
-                message="Product prices cannot be empty"
-            )
-        # Validate and add ProductPrice objects
-        product_prices = []
-        for product_price_request in req.product_prices:
-            # Validate color_id
-            stmt = select(Color).where(Color.id == product_price_request.color_id)
-            result = await session.execute(stmt)
-            color = result.scalar()
+        product_prices = await validate_and_add_product_prices(req.product_prices, product.id, session)
+        session.add_all(product_prices)
+        await session.flush()
 
-            if not color:
-                return ProductResponse(
-                    data=None,
-                    message=f"Invalid color_id: {product_price_request.color_id}. Color does not exist."
-                )
-
-            # Check if a ProductPrice already exists for the same product and color
-            stmt = select(ProductPrice).where(
-                and_(
-                    ProductPrice.product_id == product.id,
-                    ProductPrice.color_id == product_price_request.color_id
-                )
-            )
-            result = await session.execute(stmt)
-            existing_product_price = result.scalar()
-
-            if existing_product_price:
-                color_name = existing_product_price.color.name if existing_product_price.color else "Unknown Color"
-                return ProductResponse(
-                    data=None,
-                    message=f"Product price already exists with the same color {color_name} and product {product.name}"
-                )
-
-            # Add new ProductPrice to the list
-            product_prices.append(ProductPrice(
-                product_id=product.id,
-                color_id=product_price_request.color_id,
-                size=product_price_request.size,
-                price=product_price_request.price
-            ))
-
-        session.add_all(product_prices)  # Add the validated product prices to the session
-        await session.flush()  # Ensure the changes are flushed to the database
-
-        # Refresh the product with relationships, including product_prices
         await session.refresh(product, attribute_names=["category", "brand", "product_prices"])
         for product_price in product.product_prices:
             await session.refresh(product_price, attribute_names=["color"])
 
-        # Commit the transaction after all operations
         await session.commit()
 
         return ProductResponse(
             data=ProductDataResponse.from_entity(product),
             message="Product created successfully"
         )
-
+    except ValueError as e:
+        await session.rollback()
+        return ProductResponse(
+            data=None,
+            message=str(e)
+        )
     except Exception as e:
-        await session.rollback()  # Ensure any transaction is rolled back in case of error
+        await session.rollback()
         raise e
 
 
 async def update_product(id: str, req: ProductRequest, session: AsyncSession) -> ProductResponse:
     try:
-        # Fetch the existing product
         stmt = select(Product).options(
             selectinload(Product.category),
             selectinload(Product.brand),
-            selectinload(Product.product_prices).selectinload(ProductPrice.color)  # Load the color for product prices
+            selectinload(Product.product_prices).selectinload(ProductPrice.color)
         ).where(Product.id == id)
         result = await session.execute(stmt)
         product = result.scalar()
@@ -182,58 +112,28 @@ async def update_product(id: str, req: ProductRequest, session: AsyncSession) ->
                 message="Product not found"
             )
 
-        # Validate category_id
-        stmt = select(Category).where(Category.id == req.category_id)
-        result = await session.execute(stmt)
-        category = result.scalar()
-        if not category:
-            return ProductResponse(
-                data=None,
-                message="Invalid category_id. Category does not exist."
-            )
+        await validate_category(req.category_id, session)
+        await validate_brand(req.brand_id, session)
 
-        # Validate brand_id
-        stmt = select(Brand).where(Brand.id == req.brand_id)
-        result = await session.execute(stmt)
-        brand = result.scalar()
-        if not brand:
-            return ProductResponse(
-                data=None,
-                message="Invalid brand_id. Brand does not exist."
-            )
-
-        # Update product details
         product.name = req.name
         product.description = req.description
         product.attachment = req.attachment
         product.category_id = req.category_id
         product.brand_id = req.brand_id
 
-        # Set of existing ProductPrice IDs to track which ones to delete
+        # Collect existing ProductPrice IDs
         existing_product_price_ids = {pp.id for pp in product.product_prices}
 
-        # Validate product_prices not empty
-        if not req.product_prices:
-            logging.error(f"Product prices cannot be empty")
-            return ProductResponse(
-                data=None,
-                message="Product prices cannot be empty"
-            )
-        # Process new ProductPrice entries
-        new_product_prices = []
+        # Validate and process new and updated ProductPrice entries
+        new_product_prices = await validate_and_add_product_prices(
+            req.product_prices,
+            product.id,
+            session,
+            existing_product_price_ids
+        )
+
+        # Handle updates to existing ProductPrice entries
         for product_price_request in req.product_prices:
-            # Validate color_id
-            stmt = select(Color).where(Color.id == product_price_request.color_id)
-            result = await session.execute(stmt)
-            color = result.scalar()
-
-            if not color:
-                return ProductResponse(
-                    data=None,
-                    message=f"Invalid color_id: {product_price_request.color_id}. Color does not exist."
-                )
-
-            # Check if this ProductPrice entry exists
             stmt = select(ProductPrice).where(
                 and_(
                     ProductPrice.product_id == product.id,
@@ -244,12 +144,13 @@ async def update_product(id: str, req: ProductRequest, session: AsyncSession) ->
             existing_product_price = result.scalar()
 
             if existing_product_price:
-                # Update existing product price if it already exists
-                existing_product_price.size = product_price_request.size
-                existing_product_price.price = product_price_request.price
-                existing_product_price_ids.discard(existing_product_price.id)  # Remove from set
+                if existing_product_price.id in existing_product_price_ids:
+                    # Update existing ProductPrice
+                    existing_product_price.size = product_price_request.size
+                    existing_product_price.price = product_price_request.price
+                    existing_product_price_ids.remove(existing_product_price.id)
             else:
-                # Add new ProductPrice entry
+                # If not found, it should be a new ProductPrice entry
                 new_product_prices.append(ProductPrice(
                     product_id=product.id,
                     color_id=product_price_request.color_id,
@@ -257,7 +158,7 @@ async def update_product(id: str, req: ProductRequest, session: AsyncSession) ->
                     price=product_price_request.price
                 ))
 
-        # Delete product prices that are not in the updated list
+        # Delete product prices that are no longer valid
         for product_price_id in existing_product_price_ids:
             stmt = select(ProductPrice).where(ProductPrice.id == product_price_id)
             result = await session.execute(stmt)
@@ -265,25 +166,29 @@ async def update_product(id: str, req: ProductRequest, session: AsyncSession) ->
             if product_price:
                 await session.delete(product_price)
 
-        # Add new ProductPrice entries to the session
+        # Add new ProductPrice entries
         session.add_all(new_product_prices)
-        await session.flush()  # Ensure changes are flushed to the database
+        await session.flush()
 
         # Refresh the product with relationships, including product_prices
         await session.refresh(product, attribute_names=["category", "brand", "product_prices"])
         for product_price in product.product_prices:
             await session.refresh(product_price, attribute_names=["color"])
 
-        # Commit the transaction
         await session.commit()
 
         return ProductResponse(
             data=ProductDataResponse.from_entity(product),
             message="Product updated successfully"
         )
-
+    except ValueError as e:
+        await session.rollback()
+        return ProductResponse(
+            data=None,
+            message=str(e)
+        )
     except Exception as e:
-        await session.rollback()  # Roll back the transaction in case of error
+        await session.rollback()
         logging.error(f"Error updating product: {e}", exc_info=True)
         return ProductResponse(
             data=None,
@@ -307,3 +212,88 @@ async def delete_product(id: str, session: AsyncSession):
     return {
         "message": "Product deleted successfully"
     }
+
+
+async def validate_category(category_id: str, session: AsyncSession) -> Category:
+    stmt = select(Category).where(Category.id == category_id)
+    result = await session.execute(stmt)
+    category = result.scalar()
+    if not category:
+        raise ValueError("Invalid category_id. Category does not exist.")
+    return category
+
+
+async def validate_brand(brand_id: str, session: AsyncSession) -> Brand:
+    stmt = select(Brand).where(Brand.id == brand_id)
+    result = await session.execute(stmt)
+    brand = result.scalar()
+    if not brand:
+        raise ValueError("Invalid brand_id. Brand does not exist.")
+    return brand
+
+
+async def validate_color(color_id: str, session: AsyncSession) -> Color:
+    stmt = select(Color).where(Color.id == color_id)
+    result = await session.execute(stmt)
+    color = result.scalar()
+    if not color:
+        raise ValueError(f"Invalid color_id: {color_id}. Color does not exist.")
+    return color
+
+
+async def validate_product_name(name: str, session: AsyncSession) -> Product:
+    stmt = select(Product).options(
+        selectinload(Product.category),
+        selectinload(Product.brand),
+        selectinload(Product.product_prices).selectinload(ProductPrice.color)
+    ).where(Product.name == name)
+    result = await session.execute(stmt)
+    return result.scalar()
+
+
+async def validate_and_add_product_prices(
+        req_prices,
+        product_id,
+        session: AsyncSession,
+        existing_product_prices_ids=None
+):
+    product_prices = []
+    if existing_product_prices_ids is None:
+        existing_product_prices_ids = set()
+
+    # validate product must not empty
+    if not req_prices:
+        raise ValueError("Product prices cannot be empty.")
+
+    for product_price_request in req_prices:
+        color = await validate_color(product_price_request.color_id, session)
+
+        # Check if the ProductPrice entry already exists
+        stmt = select(ProductPrice).where(
+            and_(
+                ProductPrice.product_id == product_id,
+                ProductPrice.color_id == product_price_request.color_id
+            )
+        )
+        result = await session.execute(stmt)
+        existing_product_price = result.scalar()
+
+        if existing_product_price:
+            if existing_product_price.id not in existing_product_prices_ids:
+                # If it exists but is not in the list of IDs to update, raise an error
+                raise ValueError(f"Product price already exists with the same color {color.name} and product.")
+            # If it exists and is part of the IDs to update, no need to add to new entries
+            continue
+
+        # Add new ProductPrice to the list
+        product_prices.append(ProductPrice(
+            product_id=product_id,
+            color_id=product_price_request.color_id,
+            size=product_price_request.size,
+            price=product_price_request.price
+        ))
+
+    if not product_prices and not existing_product_prices_ids:
+        raise ValueError("Product prices cannot be empty.")
+
+    return product_prices
