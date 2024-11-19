83fea519df6b275b75d8f32fe9b454798f6d731b
pksrach <pksrach@gmail.com>
2024-11-19 18:36:20 +0700

    Add payment method on frontend

--- app/routes/frontend/base_frontend.py
+++ app/routes/frontend/base_frontend.py
@@ -7,6 +7,7 @@ from app.routes.frontend.frontend_color_router import frontend_color_router
 from app.routes.frontend.frontend_location_router import frontend_location_router
 from app.routes.frontend.frontend_notification_router import frontend_notification_router
 from app.routes.frontend.frontend_order_router import frontend_order_router
+from app.routes.frontend.frontend_payment_method_router import frontend_payment_method_router
 from app.routes.frontend.frontend_product_router import frontend_product_router
 from app.routes.frontend.frontend_user_profile_router import frontend_profile_router
 
@@ -20,6 +21,7 @@ frontend_router.include_router(frontend_product_router, tags=["Frontend Product 
 frontend_router.include_router(frontend_category_router, tags=["Frontend Category API"])
 frontend_router.include_router(frontend_brand_router, tags=["Frontend Brand API"])
 frontend_router.include_router(frontend_color_router, tags=["Frontend Color API"])
+frontend_router.include_router(frontend_payment_method_router, tags=["Frontend Payment Method API"])
 frontend_router.include_router(frontend_cart_router, tags=["Frontend Cart API"])
 frontend_router.include_router(frontend_order_router, tags=["Frontend Order API"])
 frontend_router.include_router(frontend_location_router, tags=["Frontend Location API"])
